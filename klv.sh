aa5e122a45f0e323e9572e7ec54f933840aa0c3b
JDeanThomas <JDeanThomas#gmail.com>
2019-08-27 16:22:38 -0400

    one liner

--- klv.sh
+++ klv.sh
@@ -6,8 +6,6 @@ read -p "Directory: " dir
 
 echo "converting files in $dir"
 
-#for i in *.avi; do ffmpeg -i "$i" "${i%.*}.mp4"; done
-
 for i in $dir/*.mpg; do ffmpeg -i "$i" -map data-re -codec copy -f data - | python3 ./klvdata_parse.py; done
 
 echo "finished"
